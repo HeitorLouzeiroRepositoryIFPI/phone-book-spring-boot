9d7d37e8a5bf533917876d06f68874b62d184d1d
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2024-12-12 22:53:32 -0300

    feat: Adiciona funcionalidades de gerenciamento de telefones, incluindo cadastro e exclusão.

--- phone-book/src/main/java/br/com/louzeiroheitor/phone_book/controller/PhoneController.java
+++ phone-book/src/main/java/br/com/louzeiroheitor/phone_book/controller/PhoneController.java
@@ -4,17 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-
 import br.com.louzeiroheitor.phone_book.models.Phones;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;   
 
 
 
 @Controller
+@RequestMapping("/phoneManagement")
 public class PhoneController {
 
     private List<Phones> phones = new ArrayList<>();
@@ -24,19 +22,26 @@ public class PhoneController {
         model.addAttribute("phone", new Phones());
         return "phoneManagement/cadastrar-telefone";
     }
+    
+    @PostMapping("/cadastrar-telefone")
+    public String postCadastrarPhone(@ModelAttribute Phones phone) {
+        phone.setId(UUID.randomUUID());
+        phones.add(phone);
+        return "redirect:/phoneManagement/listar-telefones";
+    }
 
     @GetMapping("/listar-telefones")
     public String getListarTelefones(Model model) {
         model.addAttribute("phones", phones);
         return "phoneManagement/listar-telefones";
     }
-    
 
-    @PostMapping("/cadastrar-telefone")
-    public String postCadastrarPhone(@ModelAttribute Phones phone) {
-        phone.setId(UUID.randomUUID());
-        phones.add(phone);
-        return "phoneManagement/listar-telefones";
+    @PostMapping("/{id}/deletar-telefone")
+    public String deletar(@PathVariable UUID id) {
+        phones.removeIf(phone -> phone.getId().equals(id));
+        return "redirect:/phoneManagement/listar-telefones";
     }
-    
+
+
+
 }
